84274c63f5249015662300405e6879c30c1b29fb
josephndegwa <kariukindegwa98@gmail.com>
2021-12-16 18:59:40 +0300

    add src folder

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -20,9 +20,21 @@ export class GoalComponent implements OnInit {
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
 
-  completeGoal(isComplete:any, index:any){
+  addNewGoal(goal:any){
+    let goalLength = this.goals.length;
+    goal.id = goalLength+1;
+    goal.completeDate = new Date(goal.completeDate)
+    this.goals.push(goal)
+  }
+
+  deleteGoal(isComplete:any, index:any){
     if (isComplete) {
-      this.goals.splice(index,1);
+      let toDelete = confirm(`Are you sure you want tot delete ${this.goals[index].name}?`)
+
+      if (toDelete) {
+        this.goals.splice(index,1)
+      }
+
     }
   }
 
