799c499abd321e769d4b9f74f1c1807593c5cb0e
josephndegwa <kariukindegwa98@gmail.com>
2022-01-13 23:07:41 +0300

    first commit

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,23 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { GoalService } from '../goal-service/goal.service';
 import { Goal } from '../goal';
+import { AlertService } from '../alert-service/alert.service';
+import { HttpClient } from '@angular/common/http';
+import { Quote } from '../quote-class/quote';
+import { QuoteRequestService } from '../quote-http/quote-request.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
   styleUrls: ['./goal.component.css']
 })
-export class GoalComponent implements OnInit {
+export class GoalComponent implements OnInit { 
 
-  goals:Goal[] = [
-    new Goal(1,'Watch finding Nemo','Find an online version and watch merlin find his son',new Date(2020,3,14)),
-    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
-    new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
-    new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
-    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
-  ];
 
-  toggleDetails(index:any){
-    this.goals[index].showDescription = !this.goals[index].showDescription;
+  goals:Goal[];
+  alertService:AlertService;
+  quote!: Quote;
+  
+  
+
+  goToUrl(id:any){
+    this.router.navigate(['/goals',id])
   }
 
   addNewGoal(goal:any){
@@ -27,20 +32,24 @@ export class GoalComponent implements OnInit {
     this.goals.push(goal)
   }
 
-  deleteGoal(isComplete:any, index:any){
-    if (isComplete) {
-      let toDelete = confirm(`Are you sure you want tot delete ${this.goals[index].name}?`)
+  deleteGoal(index:any){
+    let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}`)
 
-      if (toDelete) {
-        this.goals.splice(index,1)
-      }
-
+    if (toDelete){
+      this.goals.splice(index,1)
+      this.alertService.alertMe("Goal has been deleted")
     }
+  
   }
 
-  constructor() { }
+  constructor(goalService:GoalService, alertService:AlertService, private quoteService:QuoteRequestService, private router:Router) {
+    this.goals = goalService.getGoals()
+    this.alertService = alertService;
+  }
 
   ngOnInit() {
+    this.quoteService.quoteRequest()
+    this.quote = this.quoteService.quote
   }
 
 }
